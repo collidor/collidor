b601fdb7a62725be6db27289bd488aa78b39e33e
Alykam Burdzaki <alykam@gmail.com>
2026-08-09 15:01:59 +0200

    fix: skip already-published npm package versions so un-migrated packages do not block OIDC pipeline

---
+++ packages/injector/publishNpm.ts
@@ -0,0 +1,30 @@
+const denoJson = JSON.parse(await Deno.readTextFile("./package.json"));
+const pkgName = denoJson.name;
+const localVersion = denoJson.version;
+
+const viewCommand = new Deno.Command("npm", {
+  args: ["view", pkgName, "version"],
+  stdout: "piped",
+  stderr: "null",
+});
+
+const viewOutput = await viewCommand.output();
+const publishedVersion = new TextDecoder().decode(viewOutput.stdout).trim();
+
+if (publishedVersion === localVersion) {
+  console.log(`Skipping, ${pkgName}@${localVersion} is already published to npm.`);
+  Deno.exit(0);
+}
+
+console.log(`Publishing ${pkgName}@${localVersion} to npm...`);
+const publishCommand = new Deno.Command("npm", {
+  args: ["publish", "--access", "public"],
+  stdin: "inherit",
+  stdout: "inherit",
+  stderr: "inherit",
+});
+
+const publishStatus = await publishCommand.spawn().status;
+if (!publishStatus.success) {
+  Deno.exit(publishStatus.code);
+}
